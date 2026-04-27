4a870aa18753865835f33d783a855c4e47dc3cdd
Kayode David Odunayo <davidkayode153@gmail.com>
2026-04-27 10:19:49 +0100

    Python Lists Assignment task1

---
+++ task1_create_lists.py
@@ -0,0 +1,11 @@
+fruits = ['mango', 'banana', 'apple', 'orange', 'guava']
+print(fruits)
+
+programming_languages = ['python', 'rust', 'C++', 'JavaScript', 'Kotlin']
+print(programming_languages)
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(numbers)
+
+student_details = ['Kayode David Odunayo', 20, '1.75m', 'Yes']
+print(student_details)
