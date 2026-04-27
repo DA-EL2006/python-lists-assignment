01b8cc555c311eee71cc117842d1548593277f4d
Kayode David Odunayo <davidkayode153@gmail.com>
2026-04-27 11:09:46 +0100

    corrected something in task1

--- task1_create_lists.py
+++ task1_create_lists.py
@@ -7,5 +7,5 @@ print(programming_languages)
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(numbers)
 
-student_details = ['Kayode David Odunayo', 20, '1.75m', 'Yes']
+student_details = ['Kayode David Odunayo', 20, '1.75m', True]
 print(student_details)
